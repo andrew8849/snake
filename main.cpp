18696e72836b51630a157ae13aa708f11a5da83f
andrew8849 <andrew8849@kookmin.ac.kr>
2021-06-18 14:08:01 +0900

    메인

--- main.cpp
+++ main.cpp
@@ -10,55 +10,52 @@ struct map_size{
   int x;
   int y;
 };
+struct map_size map_size;
+void printScreen(char ***map);
 
 int main(){
-  WINDOW *main_screen;
-
-  struct map_size map_size;
-  map_size.x=21;
-  map_size.y=21;
-  setlocale(LC_ALL,"");
-  keypad(stdscr, TRUE);
-  map map(map_size.x,map_size.y);
-  char*** test =map.get_map();
+  WINDOW *main_screen;  // 윈도우 생성
+  map_size.x=21;  // 맵 기본사이즈 설정
+  map_size.y=21;  // 맵 기본사이즈 설정
+  setlocale(LC_ALL,""); // 특수문자 사용을위한 지역설정
+  keypad(stdscr, TRUE); // 특수 키 값 입력 허용
+  map map(map_size.x,map_size.y); // 맵 객체 생성
+  char*** test =map.get_map();  //  맵 수정을 위한 변수생성
 
   initscr();
   start_color();
   init_pair(1,COLOR_YELLOW,COLOR_BLACK);
-
   bkgd(COLOR_PAIR(1));
-  resize_term(100, 100);
-  border('*', '*', '*', '*', '*', '*', '*', '*');
-  refresh();
+  resize_term(100, 100);  // 윈도우 사이즈 설정
 
-
-
-  char Key;
-  char i = '0';
-
-  while(Key!=27){
+  char Key; // 키입력을 받기위한 변수
+  while(Key!=27){ // ESC입력시 while문 탈출
       curs_set(0);
-      timeout(0);
-      keypad(stdscr, false);
+      timeout(1);
+
       // 화면에 맵 그리기.
-      for(int i = 0 ; i<map_size.y;i++){
-        for(int j=0; j<map_size.x; j++){
-          mvprintw(i+5,(j*2)+5,test[i][j]);
-        }
-      }
+      printScreen(test);
       refresh();
+      noecho();
       Key = getch();
       while(getch()!=EOF);
-      mvprintw(0,0,&Key);
       usleep(500000);
       clear();
-      map.init_map();
-      map.item_create(false);
-      map.item_create(true);
-      map.gate_create();
+      map.init_map(); // 맵 초기화
+      map.item_create(false); // 독 생성  [뱀 길이 줄어듦]
+      map.item_create(true);  // 하트 생성   [뱀 길이 늘어남]
+      map.gate_create();  // 게이트 생성
 
   }
   endwin();
   return 0;
+}
 
+
+void printScreen(char ***map){
+  for(int i = 0 ; i<map_size.y;i++){
+    for(int j=0; j<map_size.x; j++){
+      mvprintw(i+5,(j*2)+5,map[i][j]);
+    }
+  }
 }
