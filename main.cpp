42dbbf51043252c64f9ee3aaf14a9507bae2421d
andrew8849 <andrew8849@kookmin.ac.kr>
2021-06-18 13:36:50 +0900

    메인

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include "map.cpp"
 #include <ncurses.h>
 #include <clocale>
+#include <sys/time.h>
+#include <sys/types.h>
 #include <unistd.h>
+
 
 struct map_size{
   int x;
@@ -9,7 +12,6 @@ struct map_size{
 };
 
 int main(){
-
   WINDOW *main_screen;
 
   struct map_size map_size;
@@ -17,13 +19,8 @@ int main(){
   map_size.y=21;
   setlocale(LC_ALL,"");
   keypad(stdscr, TRUE);
-  curs_set(0);
-  //noecho();
-
   map map(map_size.x,map_size.y);
   char*** test =map.get_map();
-
-
 
   initscr();
   start_color();
@@ -33,34 +30,34 @@ int main(){
   resize_term(100, 100);
   border('*', '*', '*', '*', '*', '*', '*', '*');
   refresh();
+
+
+
   char Key;
   char i = '0';
+
   while(Key!=27){
+      curs_set(0);
+      timeout(0);
+      keypad(stdscr, false);
+      // 화면에 맵 그리기.
       for(int i = 0 ; i<map_size.y;i++){
         for(int j=0; j<map_size.x; j++){
           mvprintw(i+5,(j*2)+5,test[i][j]);
         }
       }
-      mvprintw(1,1,&i);
       refresh();
+      Key = getch();
+      while(getch()!=EOF);
+      mvprintw(0,0,&Key);
+      usleep(500000);
+      clear();
       map.init_map();
-      sleep(1);
       map.item_create(false);
+      map.item_create(true);
       map.gate_create();
 
-      i++;
-      clear();
   }
-
-  main_screen = newwin(map_size.y,map_size.x,5,5);
-  wbkgd(main_screen, COLOR_PAIR(1));
-  wattron(main_screen, COLOR_PAIR(1));
-  mvwprintw(main_screen, 1, 1, "A new window");
-  wborder(main_screen, '@','@','@','@','@','@','@','@');
-  wrefresh(main_screen);
-
-  getch();
-  delwin(main_screen);
   endwin();
   return 0;
 
