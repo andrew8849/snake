430cef3f97c6f2b545d09e07bff281c40f1226b2
andrew8849 <andrew8849@kookmin.ac.kr>
2021-06-19 16:24:09 +0900

    datatype

---
+++ datatype.cpp
@@ -0,0 +1,22 @@
+struct Point{
+  int x = 0;
+  int y = 0;
+  Point(int _x, int _y) : x(_x), y(_y){};
+  Point(){};
+  bool operator==(Point p){
+    if(p.x==x && p.y==y) return true;
+    return false;
+  }
+};
+
+struct two_Point{
+  struct Point p1;
+  struct Point p2;
+  two_Point(struct Point _p1, struct Point _p2){
+    p1.x = _p1.x;
+    p1.y = _p1.y;
+    p2.x = _p2.x;
+    p2.y = _p2.y;
+  };
+  two_Point(){};
+};
